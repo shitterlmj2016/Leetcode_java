6770d23769a8a8d4dc0d49964430d6362362b697
xinchenh <915930987@qq.com>
2019-01-19 00:25:53 -0500

    1.18 就做了两道，真的难

---
+++ src/NumberofIslands.java
@@ -0,0 +1,14 @@
+/**
+ * This is a comment!
+ *
+ * @class: NumberofIslands
+ * @description: 200. Number of Islands
+ * @author: Xincheng Huang - xinchenh
+ * @create: 01-19-2019 00:25
+ **/
+public class NumberofIslands {
+    public int numIslands(char[][] grid) {
+        if (grid == null || grid.length == 0)
+            return 0;
+    }
+}
